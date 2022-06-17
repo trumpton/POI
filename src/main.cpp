287592c65746b4132a1703571bc7ebc4fcc6d872
Steve Clarke <steve@flake>
2022-06-17 16:18:37 +0100

    Added appliction icon

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    a.setWindowIcon(QIcon(":/appimage/POI.png"));
 
+    MainWindow w;
     w.show();
+
     return a.exec();
 }
